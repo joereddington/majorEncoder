b58df631813ededecdaf91aef9a19880d14b361a
Joe Reddington <joe@joereddington.com>
2023-11-06 12:40:58 +0000

    There's now a recusive brute

--- algorithm_brute.py
+++ algorithm_brute.py
@@ -2,8 +2,35 @@ import search
 import sys
 
 
+
+
+
+def recursive_number_combinations(target, unique_numbers):
+#    print("Enter the function")
+    for number in unique_numbers:
+        index = target.find(number)
+        if index != -1:
+            target_before=target[:index] 
+#            print(f"We've found {number} and now seek {target_before}")
+            results_before=[]
+            if len(target_before)>0: 
+                results_before=recursive_number_combinations(target_before,unique_numbers)
+                if len(results_before)==0:
+                    return []
+            target_after=target[index+len(number):]
+            results_after=[]
+            if len(target_after)>0: 
+                results_after=recursive_number_combinations(target_after,unique_numbers)
+                if len(results_after)==0:
+                    return []
+
+            to_return = results_before + [number] + results_after
+#            print(to_return)
+            return(to_return)
+    return [] 
+
+
 def find_number_combinations(target, unique_numbers, current=[]):
-    print(f"Target: {target}, current: {current}")
     if not target:  # if the target string is empty, a solution has been found
         yield current
         #TODO print the range of numbers
@@ -36,6 +63,21 @@ def brute_force_find(target, matches_to_return):
         counter += 1
         if counter == max_matches:
             break
+
+
+def print_table_for_number_combination(combination,words):
+        columns=[]
+        for found_number in combination: 
+            #For each number we'll find the words that match.  
+            matches=[]
+            for word, number in words.items():
+                if found_number==number:
+                   matches.append(word) 
+            #Then we'll put it into a little thing 
+            columns.append(matches)
+        #Then we print the thing
+        search.print_results_table(columns)
+
 
 def brute_force_number_find(target, matches_to_return):
     words = search.load_numbered_words("numbered_words.txt")
@@ -74,6 +116,17 @@ def brute_force_biggest(target, matches_to_return):
             if len(number)>2:
                 print(f"{number}: {word}") 
 
+def brute_force_anywhere(target): 
+    words = search.load_numbered_words("numbered_words.txt")
+    counter = 0
+    unique_numbers = set()
+    # Collect the unique numbers from the dictionary
+    for word, number in words.items():
+        unique_numbers.add(number)
+    sorted_unique_numbers = sorted(unique_numbers, key=lambda x: len(str(x)),reverse=True)
+    result= recursive_number_combinations(target,sorted_unique_numbers)
+    print_table_for_number_combination(result,words)
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("Usage: python script_name.py <digits> <max_matches>")
@@ -82,7 +135,8 @@ if __name__ == '__main__':
     target_digits = sys.argv[1].replace('.','')
     max_matches = int(sys.argv[2])
 #    brute_force_number_find(target_digits,max_matches)
-    brute_force_biggest(target_digits,max_matches)
+#    brute_force_biggest(target_digits,max_matches)
+    brute_force_anywhere(target_digits)
 
 
 # TODO 
